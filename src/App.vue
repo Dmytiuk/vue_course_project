08db16081fb3d91f733993c6e9000275947ab61d
v.dmytriuk <valaimg2218@gmail.com>
2021-04-04 04:05:38 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header/>
+    <div class="container">
+      <transition name="moveInUp">
+        <router-view/>
+      </transition>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import header from './layouts/header.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'App',
+    components: {
+      Header: header
     }
   }
-}
+</script>
+
+<style>
+  .moveInUp-enter-active{
+    animation: fadeIn 2s ease-in;
+  }
+  @keyframes fadeIn{
+    0%{
+      opacity: 0;
+    }
+    50%{
+      opacity: 0.5;
+    }
+    100%{
+      opacity: 1;
+    }
+  }
+
+  .moveInUp-leave-active{
+    animation: moveInUp .3s ease-in;
+  }
+  @keyframes moveInUp{
+    0%{
+      transform: translateY(0);
+    }
+    100%{
+      transform: translateY(-400px);
+    }
+  }
 </style>
---
+++ src/layouts/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/start">Getting Started</router-link></li>
+          <li><router-link to="/about">About Us</router-link></li>
+          <li><router-link to="/contacts">Contacts</router-link></li>
+        </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style lang="scss" scoped>
+  header{
+    display: flex;
+      nav{
+          ul{
+              display: flex;
+          }
+      }
+  }
+</style>
